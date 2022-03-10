ff36a6f5ebd1a047cc5d45b27289568b39d96860
nameless <shk.illia@gmail.com>
2022-03-10 19:42:32 +0300

    .

--- src/TestPackage/TestJavaClass.java
+++ src/TestPackage/TestJavaClass.java
@@ -1,2 +1,9 @@
-package TestPackage;public class TestJavaClass {
+package TestPackage;
+
+public class TestJavaClass {
+    public static void main(String[] args) {
+        short a = 1;
+        short b = 2;
+        System.out.println(a+b);
+    }
 }
